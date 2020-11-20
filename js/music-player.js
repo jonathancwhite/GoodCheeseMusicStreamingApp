f1c3200bce45044d0b12c14126e906c451eebf85
Jonathan White <34585279+jonathancwhite@users.noreply.github.com>
2020-11-20 14:54:19 -0600

    Add files via upload

---
+++ js/music-player.js
@@ -0,0 +1,188 @@
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      audio: null,
+      circleLeft: null,
+      barWidth: null,
+      duration: null,
+      currentTime: null,
+      isTimerPlaying: false,
+      tracks: [
+        {
+          name: "Never Gonna Give You Up",
+          artist: "Rick Astley",
+          cover: "https://imageproxy.themaven.net//https%3A%2F%2Fedm.com%2F.image%2FMTc0NjYyNjI0ODg4MjM1Mzg2%2Fscience-of-earworms-explain-why-never-gonna-give-you-up-is-stuck-in-our-heads-30-years-later.png",
+          source: "adio/rickastley_nevergonnagiveyouup.mp3",
+          favorited: false
+        },
+        {
+          name: "Never Fall Off",
+          artist: "SmooveLee",
+          cover: "http://blackforezt.com/testing-base/img/ifsvol2_albumartwork.png",
+          source: "http://blackforezt.com/audio/ifs_vol2/NeverFallOff.mp3",
+          favorited: false
+        },
+        {
+          name: "GodL33",
+          artist: "SmooveLee",
+          cover: "http://blackforezt.com/testing-base/img/ifsvol2_albumartwork.png",
+          source: "http://blackforezt.com/audio/ifs_vol2/GodLee.mp3",
+          favorited: false
+        },
+        {
+          name: "Kross Paths (Ft. Blocka)",
+          artist: "SmooveLee",
+          cover: "http://blackforezt.com/testing-base/img/ifsvol2_albumartwork.png",
+          source: "http://blackforezt.com/audio/ifs_vol2/krosspaths.mp3",
+          favorited: false
+        },
+        {
+          name: "Pure",
+          artist: "SmooveLee",
+          cover: "http://blackforezt.com/testing-base/img/ifsvol2_albumartwork.png",
+          source: "http://blackforezt.com/audio/ifs_vol2/Pure.mp3",
+          favorited: true
+        },
+        {
+          name: "Did It Again",
+          artist: "SmooveLee",
+          cover: "http://blackforezt.com/testing-base/img/ifsvol2_albumartwork.png",
+          source: "http://blackforezt.com/audio/ifs_vol2/diditagain.mp3",
+          favorited: false
+        },
+        {
+          name: "Conclusion",
+          artist: "SmooveLee",
+          cover: "http://blackforezt.com/testing-base/img/ifsvol2_albumartwork.png",
+          source: "http://blackforezt.com/audio/ifs_vol2/Conclusion.mp3",
+          favorited: false
+        }
+      ],
+      currentTrack: null,
+      currentTrackIndex: 0,
+      transitionName: null
+    };
+  },
+  methods: {
+    play() {
+      if (this.audio.paused) {
+        this.audio.play();
+        this.isTimerPlaying = true;
+      } else {
+        this.audio.pause();
+        this.isTimerPlaying = false;
+      }
+    },
+    generateTime() {
+      let width = (100 / this.audio.duration) * this.audio.currentTime;
+      this.barWidth = width + "%";
+      this.circleLeft = width + "%";
+      let durmin = Math.floor(this.audio.duration / 60);
+      let dursec = Math.floor(this.audio.duration - durmin * 60);
+      let curmin = Math.floor(this.audio.currentTime / 60);
+      let cursec = Math.floor(this.audio.currentTime - curmin * 60);
+      if (durmin < 10) {
+        durmin = "0" + durmin;
+      }
+      if (dursec < 10) {
+        dursec = "0" + dursec;
+      }
+      if (curmin < 10) {
+        curmin = "0" + curmin;
+      }
+      if (cursec < 10) {
+        cursec = "0" + cursec;
+      }
+      this.duration = durmin + ":" + dursec;
+      this.currentTime = curmin + ":" + cursec;
+    },
+    updateBar(x) {
+      let progress = this.$refs.progress;
+      let maxduration = this.audio.duration;
+      let position = x - progress.offsetLeft;
+      let percentage = (100 * position) / progress.offsetWidth;
+      if (percentage > 100) {
+        percentage = 100;
+      }
+      if (percentage < 0) {
+        percentage = 0;
+      }
+      this.barWidth = percentage + "%";
+      this.circleLeft = percentage + "%";
+      this.audio.currentTime = (maxduration * percentage) / 100;
+      this.audio.play();
+    },
+    clickProgress(e) {
+      this.isTimerPlaying = true;
+      this.audio.pause();
+      this.updateBar(e.pageX);
+    },
+    prevTrack() {
+      this.transitionName = "scale-in";
+      this.isShowCover = false;
+      if (this.currentTrackIndex > 0) {
+        this.currentTrackIndex--;
+      } else {
+        this.currentTrackIndex = this.tracks.length - 1;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    nextTrack() {
+      this.transitionName = "scale-out";
+      this.isShowCover = false;
+      if (this.currentTrackIndex < this.tracks.length - 1) {
+        this.currentTrackIndex++;
+      } else {
+        this.currentTrackIndex = 0;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    resetPlayer() {
+      this.barWidth = 0;
+      this.circleLeft = 0;
+      this.audio.currentTime = 0;
+      this.audio.src = this.currentTrack.source;
+      setTimeout(() => {
+        if(this.isTimerPlaying) {
+          this.audio.play();
+        } else {
+          this.audio.pause();
+        }
+      }, 300);
+    },
+    favorite() {
+      this.tracks[this.currentTrackIndex].favorited = !this.tracks[
+        this.currentTrackIndex
+      ].favorited;
+    }
+  },
+  created() {
+    let vm = this;
+    this.currentTrack = this.tracks[0];
+    this.audio = new Audio();
+    this.audio.src = this.currentTrack.source;
+    this.audio.ontimeupdate = function() {
+      vm.generateTime();
+    };
+    this.audio.onloadedmetadata = function() {
+      vm.generateTime();
+    };
+    this.audio.onended = function() {
+      vm.nextTrack();
+      this.isTimerPlaying = true;
+    };
+
+    // this is optional (for preload covers)
+    for (let index = 0; index < this.tracks.length; index++) {
+      const element = this.tracks[index];
+      let link = document.createElement('link');
+      link.rel = "prefetch";
+      link.href = element.cover;
+      link.as = "image"
+      document.head.appendChild(link)
+    }
+  }
+});
